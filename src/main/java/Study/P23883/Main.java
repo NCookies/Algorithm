adf6691f6b604d04127446fc6cc6e022ac9058f0
NCookies <swstar21c@gmail.com>
2024-04-29 21:49:25 +0900

    [baekjoon] P23883 문제 해결
    - 이전 커밋에서 말했던 것처럼 저장과 동시에 정렬이 수행되는 TreeMap 자료구조를 사용해 문제를 해결했다.
    - 실행 시간 1416ms

--- src/main/java/Study/P23883/Main.java
+++ src/main/java/Study/P23883/Main.java
@@ -3,7 +3,7 @@ package Study.P23883;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
+import java.util.Collections;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
@@ -17,53 +17,43 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        // 각 숫자가 몇 번째 인덱스에 있는지 저장하고 있는 배열
-        TreeMap<Integer, Integer> idxMap = new TreeMap<>();
+        // 각 숫자가 몇 번째 인덱스에 있는지 저장하고 있는 TreeMap 객체
+        TreeMap<Integer, Integer> idxMap = new TreeMap<>(Collections.reverseOrder());
 
         // 수열 A를 저장하고 있는 배열
         int[] numArr = new int[n + 1];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            int num = Integer.parseInt(st.nextToken());;
+            int num = Integer.parseInt(st.nextToken());
             idxMap.put(num, i);
             numArr[i] = num;
         }
-        
+
         int times = 0;      // 교환 횟수
+        int i = n;
+        
+        // TreeMap 객체 생성 시 내림차순으로 정렬되게 설정하였으므로 maxValue는 큰 수부터 오게 된다.
+        for (Integer maxValue : idxMap.keySet()) {
+            // 가장 큰 값이 저장된 인덱스
+            int maxIndex = idxMap.get(maxValue);
 
-        // selection sort
-        for (int i = n; i > 0; i--) {
-            // 수열 A 중 가장 큰 값을 key로, 인덱스를 value로 가지는 Map 변수 반환
-            Map.Entry<Integer, Integer> max = idxMap.pollLastEntry();
-
-            // i 위치의 값이 max가 아닌 경우
-            if (numArr[i] != max.getKey()) {
+            // i번째 데이터가 최대값이 아니라면...
+            if (numArr[i] != maxValue) {
+                // swap이 일어날 때만 카운트
                 if (++times >= k) {
-                    sb.append(numArr[i]).append(" ").append(i);
+                    // 항상 큰 값이 뒤로 오도록 정답 출력
+                    sb.append(numArr[i]).append(" ").append(maxValue);
                     System.out.println(sb);
                     return;
                 }
 
-                int tmp = idxMap.get(numArr[i]);    // 5
-                idxMap.put(numArr[i], max.getValue());  // 4
-                idxMap.put(max.getKey(), tmp);      // 5
-
-                tmp = numArr[max.getValue()];
-                numArr[max.getValue()] = numArr[i];
-                numArr[i] = tmp;
-
-//                idxMap.put(numArr[i], max.getValue());
-//                numArr[max.getValue()] = numArr[i];
-
-//                int tmp = idxArr[numArr[i]];
-//                idxArr[numArr[i]] = idxArr[i];
-//                idxArr[i] = tmp;
-//
-//                tmp = numArr[maxIndex];
-//                numArr[maxIndex] = numArr[i];
-//                numArr[i] = tmp;
+                // TreeMap과 배열 값 각각 swap
+                idxMap.put(numArr[i], maxIndex);
+                numArr[maxIndex] = numArr[i];
             }
+
+            i--;
         }
 
         System.out.println(-1);
