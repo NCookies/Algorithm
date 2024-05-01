afc90166d9f502113496ea23bdc36386a624ecd8
NCookies <swstar21c@gmail.com>
2024-05-01 17:06:31 +0900

    [baekjoon] 이전 문제 입출력 개선

--- src/main/java/Level/Level01_입출력과_사칙연산/P10430/Main.java
+++ src/main/java/Level/Level01_입출력과_사칙연산/P10430/Main.java
@@ -1,15 +1,20 @@
 package Level.Level01_입출력과_사칙연산.P10430;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int a, b, c;
 
-        Scanner scanner = new Scanner(System.in);
-        a = scanner.nextInt();
-        b = scanner.nextInt();
-        c = scanner.nextInt();
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
 
         System.out.println((a + b) % c);
         System.out.println(((a % c) + (b + c)) % c);
