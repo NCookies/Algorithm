0e5ad52ec3eac5f6246bcd4b243bbda0338bd7a6
NCookies <swstar21c@gmail.com>
2024-05-09 23:27:38 +0900

    [baekjoon] P2751 문제 해결
    - 찾아본 결과 애초에 이 문제는 퀵 정렬로 푸는 것이 권장되지 않는다고 한다. 퀵 정렬 최악의 케이스가 나오도록 저격 데이터를 넣어두었다고...
    - 그래서 퀵 정렬 대신에 병합 정렬, 힙 정렬 등을 쓰거나, 자바의 내장 함수인 Collection.sort() 메소드를 쓰라고 한다.
    - https://www.acmicpc.net/board/view/31887

--- src/main/java/Study/Chapter04_퀵정렬/P2751/Main.java
+++ src/main/java/Study/Chapter04_퀵정렬/P2751/Main.java
@@ -3,66 +3,87 @@ package Study.Chapter04_퀵정렬.P2751;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        ArrayList<Integer> list = new ArrayList<>();
+
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
 
         for (int i = 0 ; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            list.add(Integer.parseInt(br.readLine()));
         }
 
-        quickSort(arr, 0, n - 1);
-
-        for (int i = 0 ; i < n; i++) {
-            sb.append(arr[i]).append("\n");
+        Collections.sort(list);
+        for(Integer i : list) {
+            sb.append(i).append("\n");
         }
+
         System.out.println(sb);
     }
-
-    private static void quickSort(int[] arr, int lo, int hi) {
-        if (lo >= hi) {
-            return;
-        }
-
-        int pivot = partition(arr, lo, hi);
-
-        quickSort(arr, lo, pivot);
-        quickSort(arr, pivot + 1, hi);
-    }
-
-    private static int partition(int[] arr, int lo, int hi) {
-        // 피벗으로 중간값 선택
-        // int 값의 오버플로우를 대비하여 다음과 같이 수식을 구성함
-        int pivot = arr[lo + ((hi - lo) / 2)];
-        lo--; hi++;
-
-        while (lo <= hi) {
-            do {
-                lo++;
-            } while (arr[lo] < pivot);
-
-            do {
-                hi--;
-            } while (arr[hi] > pivot && lo <= hi);
-
-            if (lo < hi) {
-                swap(arr, lo, hi);
-            } else {
-                break;
-            }
-        }
-
-        return hi;
-    }
-
-    private static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        int n = Integer.parseInt(br.readLine());
+//        int[] arr = new int[n];
+//
+//        for (int i = 0 ; i < n; i++) {
+//            arr[i] = Integer.parseInt(br.readLine());
+//        }
+//
+//        quickSort(arr, 0, n - 1);
+//
+//        for (int i = 0 ; i < n; i++) {
+//            sb.append(arr[i]).append("\n");
+//        }
+//        System.out.println(sb);
+//    }
+//
+//    private static void quickSort(int[] arr, int lo, int hi) {
+//        if (lo >= hi) {
+//            return;
+//        }
+//
+//        int pivot = partition(arr, lo, hi);
+//
+//        quickSort(arr, lo, pivot);
+//        quickSort(arr, pivot + 1, hi);
+//    }
+//
+//    private static int partition(int[] arr, int lo, int hi) {
+//        // 피벗으로 중간값 선택
+//        // int 값의 오버플로우를 대비하여 다음과 같이 수식을 구성함
+//        int pivot = arr[lo + ((hi - lo) / 2)];
+//        lo--; hi++;
+//
+//        while (lo <= hi) {
+//            do {
+//                lo++;
+//            } while (arr[lo] < pivot);
+//
+//            do {
+//                hi--;
+//            } while (arr[hi] > pivot && lo <= hi);
+//
+//            if (lo < hi) {
+//                swap(arr, lo, hi);
+//            } else {
+//                break;
+//            }
+//        }
+//
+//        return hi;
+//    }
+//
+//    private static void swap(int[] arr, int i, int j) {
+//        int tmp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = tmp;
+//    }
 }
