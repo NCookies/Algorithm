d18c09dc7b26fc9bde9d77d64aee1cdb45567fe2
NCookies <swstar21c@gmail.com>
2024-05-15 01:28:28 +0900

    [baekjoon] P24092 시간 초과

---
+++ src/main/java/Study/Chapter04_퀵정렬/P24092/Main.java
@@ -0,0 +1,96 @@
+package Study.Chapter04_퀵정렬.P24092;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arrA;
+    static int[] arrB;
+
+    static boolean flag;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        arrA = new int[n];
+        arrB = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arrA[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arrB[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (compare()) {
+            return;
+        }
+
+        quickSort(arrA, 0, n - 1);
+
+        if (!flag) {
+            System.out.println(0);
+        }
+    }
+
+    static void quickSort(int[] a, int lo, int hi) {
+        if (lo < hi) {
+            int pivot = partition(a, lo, hi);
+            if (pivot == -1) {
+                return;
+            }
+
+            quickSort(a, lo, pivot - 1);
+            quickSort(a, pivot + 1, hi);
+        }
+    }
+
+    static int partition(int[] a, int lo, int hi) {
+        int pivot = a[hi];
+        int i = lo - 1;
+
+        for (int j = lo; j < hi; j++) {
+            if (a[j] <= pivot) {
+                swap(a, ++i, j);
+
+                if (compare()) {
+                    return -1;
+                }
+            }
+        }
+
+        if (i + 1 != hi) {
+            swap(a, i + 1, hi);
+
+            if (compare()) {
+                return -1;
+            }
+        }
+
+        return i + 1;
+    }
+
+    static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    static boolean compare() {
+        for (int i = 0; i < arrA.length; i++) {
+            if (arrA[i] != arrB[i]) {
+                return false;
+            }
+        }
+        flag = true;
+        System.out.println(1);
+        return true;
+    }
+}
