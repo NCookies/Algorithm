91a52a760360d036dc6dfb88e21131d33bdf0930
NCookies <swstar21c@gmail.com>
2024-05-16 22:49:27 +0900

    [baekjoon] P1764 문제 해결 (2)
    - HashMap 자료구조를 사용하지 않고 문제를 해결했다.
    - 모든 입력 데이터를 하나의 배열에 저장하고 정렬했다. 이후 배열을 순회하면서 같은 문자열이 두 번 이상 나오면 정답에 추가하도록 했다.
    - 결과적으로 HashMap을 썼을 때보다 조금 더 빨라지긴 했지만 의미 있을 정도의 차이는 아니었다.
    - 실행 시간 : 280ms

--- src/main/java/Study/Chapter5_해시/P1764/Main.java
+++ src/main/java/Study/Chapter5_해시/P1764/Main.java
@@ -13,27 +13,27 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        int all = n + m;
 
-        HashMap<String, Boolean> stringBooleanHashMap = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            stringBooleanHashMap.put(br.readLine(), true);
+        // 모든 입력 데이터를 한 배열에 저장하고 정렬한다.
+        String[] arr = new String[all];
+        for (int i = 0; i < all; i++) {
+            arr[i] = br.readLine();
         }
+        Arrays.sort(arr);
 
-        List<String> arr = new ArrayList<>();
-        for (int j = 0; j < m; j++) {
-            String s = br.readLine();
-            if (stringBooleanHashMap.containsKey(s)) {
-                arr.add(s);
+        // 정렬한 데이터 중 같은 문자열이 2개 이상이면 정답에 추가
+        String prev = arr[0];
+        int cnt = 0;
+        for (int i = 1; i < all; i++) {
+            if (prev.equals(arr[i])) {
+                sb.append(arr[i]).append("\n");
+                cnt++;
             }
+            prev = arr[i];
         }
 
-        Collections.sort(arr);
-
-        sb.append(arr.size()).append("\n");
-        for (String s : arr) {
-            sb.append(s).append("\n");
-        }
+        System.out.println(cnt);
         System.out.println(sb);
     }
 }
