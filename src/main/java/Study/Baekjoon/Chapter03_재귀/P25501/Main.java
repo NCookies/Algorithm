4bb27689cafcdf09ce11e2b6de04e358b0683520
NCookies <swstar21c@gmail.com>
2024-05-17 23:25:34 +0900

    [baekjoon] 패키지 구조 변경
    - 프로그래머스 문제 코드도 넣기 위해 패키지 구조를 변경했다.

---
+++ src/main/java/Study/Baekjoon/Chapter03_재귀/P25501/Main.java
@@ -0,0 +1,37 @@
+package Study.Baekjoon.Chapter03_재귀.P25501;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int depth;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            depth = 1;
+            sb.append(isPalindrome(br.readLine())).append(" ").append(depth).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length() - 1);
+    }
+
+    public static int recursion(String s, int l, int r){
+        if (l >= r) return 1;
+        else if (s.charAt(l) != s.charAt(r)) return 0;
+        else {
+            depth++;
+            return recursion(s, l + 1, r - 1);
+        }
+    }
+}
---
+++ src/main/java/Study/Baekjoon/Chapter04_퀵정렬/P2751/Main.java
@@ -0,0 +1,73 @@
+package Study.Baekjoon.Chapter04_퀵정렬.P2751;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0 ; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.shuffle(list);
+
+        quickSort(list, 0, n - 1);
+
+        for (int i = 0 ; i < n; i++) {
+            sb.append(list.get(i)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void quickSort(ArrayList<Integer> arr, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+
+        int pivot = partition(arr, lo, hi);
+
+        quickSort(arr, lo, pivot);
+        quickSort(arr, pivot + 1, hi);
+    }
+
+    private static int partition(ArrayList<Integer> arr, int lo, int hi) {
+        int mid = lo + (hi - lo) / 2;
+
+        // 피벗으로 중간값 선택
+        // int 값의 오버플로우를 대비하여 다음과 같이 수식을 구성함
+        int pivot = arr.get(mid);
+        lo--; hi++;
+
+        while (lo <= hi) {
+            do {
+                lo++;
+            } while (arr.get(lo) < pivot);
+
+            do {
+                hi--;
+            } while (arr.get(hi) > pivot && lo <= hi);
+
+            if (lo < hi) {
+                swap(arr, lo, hi);
+            } else {
+                break;
+            }
+        }
+
+        return hi;
+    }
+
+    private static void swap(ArrayList<Integer> arr, int i, int j) {
+        int tmp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, tmp);
+    }
+}
