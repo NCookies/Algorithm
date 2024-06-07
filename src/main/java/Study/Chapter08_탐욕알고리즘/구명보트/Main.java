1ab0a5cde66eed890073f4635c371b28a4f3e4a4
NCookies <swstar21c@gmail.com>
2024-06-08 02:17:13 +0900

    [프로그래머스] 구명보트 문제 해결 완료
    - 보트에는 2명까지만 탈 수 있으므로 가장 효율적으로 보트에 태우는 방법은 가장 가벼운 사람과 무거운 사람을 같이 태우는 것이다.
    - 만약 한 사람만 탈 수 있다면 무거운 사람을 우선적으로 태운다.

--- src/main/java/Study/Chapter08_탐욕알고리즘/구명보트/Main.java
+++ src/main/java/Study/Chapter08_탐욕알고리즘/구명보트/Main.java
@@ -9,29 +9,25 @@ public class Main {
     }
 
     static public int solution(int[] people, int limit) {
-        int answer = 1;
+        int answer = 0;
         
         // 오름차순으로 정렬
         Arrays.sort(people);
 
-
-        int boatPeople = 0;
-        int weight = 0;
-        for (int i = people.length - 1; i >= 0; i--) {
-            if (weight + people[i] <= limit) {
-                weight += people[i];
-                boatPeople++;
-            } else {
-                weight = people[i];
+        // 보트에는 최대 2명까지만 탈 수 있다.
+        // 따라서 가장 가벼운 사람과 가장 무거운 사람을 태운다.
+        int l = 0, r = people.length - 1;
+        while (l <= r) {
+            // 가장 가벼운 사람과 무거운 사람 모두 탈 수 있는 경우
+            if (people[l] + people[r] <= limit) {
+                l++;
+                r--;
                 answer++;
-                boatPeople = 0;
             }
-
-            // 보트는 최대 2명까지만 탑승 가능
-            if (boatPeople == 2) {
-                weight = 0;
+            // 그렇지 않다면 무거운 사람만 먼저 태운다.
+            else {
+                r--;
                 answer++;
-                boatPeople = 0;
             }
         }
         
