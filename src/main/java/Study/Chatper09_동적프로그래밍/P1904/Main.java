72d9519a0bae0491952795246fbbac77c696f7b2
NCookies <swstar21c@gmail.com>
2024-06-13 10:55:18 +0900

    [백준] 1904번 문제 풀이 완료
    - 복잡한 규칙이 있는게 아니라, n이 1씩 늘어남에 따라 수열은 피보나치와 같은 규칙을 보인다.
    - 단순히 이런 피보나치 수열을 동적 프로그래밍 기법으로 풀면 되는 문제였다.

--- src/main/java/Study/Chatper09_동적프로그래밍/P1904/Main.java
+++ src/main/java/Study/Chatper09_동적프로그래밍/P1904/Main.java
@@ -8,20 +8,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int answer = 0;
-        int zeroCnt = 2;
 
-        while (zeroCnt <= n) {
-            answer += (n - (zeroCnt - 1)) % 15746;
-            zeroCnt += 2;
+        System.out.println(tile(n));
+    }
+
+    static public int tile(int x) {
+        if (x == 1) {
+            return 1;
+        }
+        if (x == 2) {
+            return 2;
         }
 
-        // n이 짝수이면 전부 1일 경우만 추가
-        // n이 홀수면 전부 0이거나 전부 1인 경우 추가
-        if (n % 2 == 0) {
-            System.out.println(answer + 1);
-        } else {
-            System.out.println(answer + 2);
+        int a = 1;
+        int b = 2;
+        int sum = 0;
+
+        for (int i = 2; i < x; i++) {
+            sum = (a + b) % 15746;
+            a = b;
+            b = sum;
         }
+
+        return sum;
     }
 }
