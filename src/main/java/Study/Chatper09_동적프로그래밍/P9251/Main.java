1cc66ae48cabb921186c350fa4add68eaf5fcba7
NCookies <swstar21c@gmail.com>
2024-06-13 12:00:00 +0900

    [백준] 9251번 문제 풀이 완료
    - https://velog.io/@emplam27/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B7%B8%EB%A6%BC%EC%9C%BC%EB%A1%9C-%EC%95%8C%EC%95%84%EB%B3%B4%EB%8A%94-LCS-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Longest-Common-Substring%EC%99%80-Longest-Common-Subsequence 블로그 참고

---
+++ src/main/java/Study/Chatper09_동적프로그래밍/P9251/Main.java
@@ -0,0 +1,34 @@
+package Study.Chatper09_동적프로그래밍.P9251;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String b = br.readLine();
+
+        dp = new int[a.length() + 1][b.length() + 1];
+
+        // 최장 공통 부분 수열 길이 탐색
+        lcs(a, b);
+
+        System.out.println(dp[a.length()][b.length()]);
+    }
+
+    static void lcs(String a, String b) {
+        for (int i = 1; i < a.length() + 1; i++) {
+            for (int j = 1; j < b.length() + 1; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j -1]);
+                }
+            }
+        }
+    }
+}
