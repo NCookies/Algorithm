5f7c888fa2432cce89a97b49c705c0ee39af960c
NCookies <swstar21c@gmail.com>
2024-06-13 12:34:50 +0900

    [백준] 12865번 문제 코드 수정
    - 이 문제의 경우 물건의 정보를 계속 저장하고 있을 필요가 없다. 때문에 루프가 돌 때마다 물건의 정보를 입력 받아 연산하도록 했다.
    - 그러나 실행 시간은 이전보다 늘어났다(...)

--- src/main/java/Baekjoon_Level/Level23_동적계획법1/P12865/Main.java
+++ src/main/java/Baekjoon_Level/Level23_동적계획법1/P12865/Main.java
@@ -7,15 +7,6 @@ import java.util.StringTokenizer;
 
 // 평범한 배낭
 public class Main {
-    static class Stuff {
-        int weight;
-        int value;
-
-        public Stuff(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,22 +17,20 @@ public class Main {
         // 최대 무게
         int k = Integer.parseInt(st.nextToken());
 
-        // 물건 정보 입력
-        Stuff[] stuff = new Stuff[n];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            stuff[i] = new Stuff(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        }
-
         int[][] backpack = new int[n + 1][k + 1];
         for (int i = 1; i < n + 1; i++) {
+            // 물건 정보 입력
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            
             for (int j = 1; j < k + 1; j++) {
                 // 두 개의 항목 중 더 큰 값을 대입한다.
                 // 1. backpack[i-1][j]
                 // 2. 현재 물건의 가치 + 남은 공간의 가치(backpack[i-1][j-현재 물건의 무게])
-                if (stuff[i - 1].weight <= j) {
+                if (weight <= j) {
                     backpack[i][j] = Math.max(backpack[i - 1][j],
-                            stuff[i - 1].value + backpack[i - 1][j - stuff[i - 1].weight]);
+                            value + backpack[i - 1][j - weight]);
                 } else {
                     backpack[i][j] = backpack[i - 1][j];
                 }
