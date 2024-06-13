b06aeed5415d2951065a42b1c3760225135f02ba
NCookies <swstar21c@gmail.com>
2024-06-14 01:43:24 +0900

    [프로그래머스] 등굣길 문제 풀이 실패 (2)
    - 문제의 접근을 달리해 다이나믹 프로그래밍으로 문제를 풀었으나 런타임 에러로 실패함
    - 접근 방법 자체는 맞으나 코드에 틀린 부분이 있는듯

--- src/main/java/Study/Chatper09_동적프로그래밍/등굣길/Main.java
+++ src/main/java/Study/Chatper09_동적프로그래밍/등굣길/Main.java
@@ -1,62 +1,46 @@
 package Study.Chatper09_동적프로그래밍.등굣길;
 
 public class Main {
-    static class Point {
-        int x;
-        int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    static int[][] dp;
-    static int min = Integer.MAX_VALUE;     // 최단거리
-    static int cnt = 0;     // 최단경로 개수
-
     public static void main(String[] args) {
         int[][] puddles = {{2, 2}};
+        int[][] puddles2 = {{1, 3}};
+
         System.out.println(solution(4, 3, puddles));
+        System.out.println(solution(4, 3, puddles2));
     }
 
     static public int solution(int m, int n, int[][] puddles) {
-        dp = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
+        
+        // 물에 잠긴 곳은 -1로 표시
         for (int[] puddle : puddles) {
             dp[puddle[0]][puddle[1]] = -1;
         }
 
-        dfs(new Point(1, 1), 0);
+        // 시작점(dp[1][1])을 1로 초기화하기 위해 설정
+        dp[0][1] = 1;
+        int sum = 0;
 
-        return cnt;
-    }
+        // 현재 위치로 올 수 있는 경로의 개수는 dp 배열의 위와 왼쪽의 값을 더한 값이다.
+        // 만약 -1일 경우에는 합 연산에서 제외한다.
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (dp[i][j] == -1) {
+                    continue;
+                }
 
-    static void dfs(Point p, int depth) {
-        // 좌표 벗어나면 리턴
-        if (p.x > dp.length - 1|| p.y > dp[0].length - 1) {
-            return;
-        }
+                sum = 0;
+                if (dp[i - 1][j] != -1) {
+                    sum += dp[i - 1][j];
+                }
+                if (dp[i][j - 1] != -1) {
+                    sum += dp[i][j - 1];
+                }
 
-        // 가지 못하는 길임
-        if (dp[p.x][p.y] == -1) {
-            return;
-        }
-
-        dp[p.x][p.y] = ++depth;
-
-        // 학교에 도달했을 때 최단거리와 최단경로 개수 갱신
-        if (p.x == dp.length - 1 && p.y == dp[0].length - 1) {
-            if (depth == min) {
-                cnt++;
-            } else if (depth < min) {
-                min = depth;
-                cnt = 1;
+                dp[i][j] = sum % 1000000007;
             }
-
-            return;
         }
 
-        dfs(new Point(p.x + 1, p.y), depth + 1);
-        dfs(new Point(p.x , p.y + 1), depth + 1);
+        return dp[n][m];
     }
 }
