5219260673517fbd62dcd4f174c0074c94bcede8
SeungWoo Ryu <swstar21c@gmail.com>
2025-03-04 10:39:08 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        double answer = sum / numbers.length;
+        return answer;
+    }
+}
