43c02f95dae648fae619a9e89a192b2ff9f514ae
SeungWoo Ryu <swstar21c@gmail.com>
2025-03-06 10:27:32 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,5 +1,14 @@
 class Solution {
     public int solution(int n) {
-        return (n / 2) * (n / 2 + 1);
+        int answer = 0;
+        
+        // 2로 나눴을 때 몫이 짝수일 때
+        if ((n / 2) % 2 == 0) {
+            answer = (n + 2) * (n / 4);
+        } else {
+            answer = (n + 2) * (n / 4) + (n / 2 + 1);
+        }
+        
+        return answer;
     }
 }
