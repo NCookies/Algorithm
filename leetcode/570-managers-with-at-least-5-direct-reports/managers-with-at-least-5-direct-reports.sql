ad8143457661d6a268bb80571f6e6ecfd0df4aac
SeungWoo Ryu <swstar21c@gmail.com>
2025-04-21 09:08:36 +0900

    Time: 345 ms (83.40%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ leetcode/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,16 @@
-SELECT
-    name
-FROM
-    Employee
-WHERE
-    id IN (
-        SELECT managerId 
-        FROM Employee 
-        GROUP BY managerId 
-        HAVING COUNT(*) >= 5)
+-- SELECT
+--     name
+-- FROM
+--     Employee
+-- WHERE
+--     id IN (
+--         SELECT managerId 
+--         FROM Employee 
+--         GROUP BY managerId 
+--         HAVING COUNT(*) >= 5)
+
+SELECT e.name
+FROM Employee e
+    INNER JOIN Employee m ON e.id = m.managerId
+GROUP BY m.managerId
+HAVING COUNT(*) >= 5
