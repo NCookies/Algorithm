fdd8090d81c72e2f4554cef0a2973f8a2e9e6db2
SeungWoo Ryu <swstar21c@gmail.com>
2025-05-19 09:13:00 +0900

    Time: 946 ms (74.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+SELECT
+    contest_id,
+    ROUND(COUNT(*) / (SELECT COUNT(*) FROM Users) * 100, 2) AS percentage
+FROM
+    Register r
+GROUP BY
+    contest_id
+ORDER BY
+    percentage DESC, contest_id
